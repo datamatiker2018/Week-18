3de65fb41635b76ab783abca22a1d89b10deb980
Simon Bo Dall Mikkelsen <simonbodall@gmail.com>
2018-05-01 15:41:12 +0200

    Add ReverseInPlace

---
+++ ReverseInPlace.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Week14Repetition.Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 1, 2, 3 };
+
+            Console.WriteLine(string.Join(", ", a));
+            
+            for (int i = 0; i < a.Length / 2; i++)
+            {
+                int swapIndex = a.Length - 1 - i;
+                int temporary = a[i];
+                a[i] = a[swapIndex];
+                a[swapIndex] = temporary;
+            }
+
+            Console.WriteLine(string.Join(", ", a));
+        }
+    }
+}
